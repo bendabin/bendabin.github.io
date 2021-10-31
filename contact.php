c6c3e61b8a3087c26e66762d85c7d12b3fe514db
bendabin <dabin100@gmail.com>
2021-10-31 13:53:06 +0900

    finished the website styling and uploaded to github ready for publishing

---
+++ contact.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+
+<h1>Submitted successfully</h1>
+
+
+<!-- echo "<h1>Email address is {$email_address}</h1>"
+echo "<h1>Email address is {$user_name}</h1>" -->
+</body>
+</html>
+
